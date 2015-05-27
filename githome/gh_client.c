7c3fa8ab5e039816d582c5966191028d3d77526e
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-27 10:08:50 +0200

    Added dry-run option.

--- githome/gh_client.c
+++ githome/gh_client.c
@@ -1,4 +1,5 @@
 #include <libgen.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -24,12 +25,51 @@ int send_all(int socket, void *buf, size_t len) {
 }
 
 
+int readline(int fd, void *buffer, size_t n) {
+  char *buf = (char*) buf;
+  ssize_t r;
+
+  --n;  /* space for terminating \0 */
+
+  do {
+    r = read(fd, buf, 1);
+    printf("read 1 byte\n");
+
+    if (r == -1) {
+        if (errno == EINTR) continue;  /* retry */
+        return -1;
+    }
+
+    if (r == 1) {
+      if (*buf == '\n') break;
+      ++buf;
+      --n;
+    }
+  } while(r && n);
+
+  *buf = '\0';
+
+  return 0;
+}
+
+
 int main(int argc, char **argv) {
   int sock = socket(AF_UNIX, SOCK_STREAM, 0);
   struct sockaddr_un srv;
 
-  if (argc < 2) {
-    fprintf(stderr, "usage: %s SOCKET [ARGS]...\n", basename(argv[0]));
+  int c, dry_run = 0;
+
+  /* parse options */
+  while((c = getopt(argc, argv,  "n")) != -1) {
+    switch(c) {
+      case 'n':
+        dry_run = 1;
+      break;
+    }
+  }
+
+  if (argc < optind + 1) {
+    fprintf(stderr, "usage: %s [-n] SOCKET [ARGS]...\n", basename(argv[0]));
     exit(1);
   }
 
@@ -39,7 +79,7 @@ int main(int argc, char **argv) {
   }
 
   srv.sun_family = AF_UNIX;
-  strncpy(srv.sun_path, argv[1], sizeof(srv.sun_path) - 1);
+  strncpy(srv.sun_path, argv[optind], sizeof(srv.sun_path) - 1);
 
   if (connect(sock, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
     close(sock);
@@ -48,7 +88,7 @@ int main(int argc, char **argv) {
   }
 
   int i;
-  for (i = 2; i < argc; ++i) {
+  for (i = optind+1; i < argc; ++i) {
     if (send_all(sock, argv[i], strlen(argv[i]))) {
       perror("failed to send");
       return 1;
