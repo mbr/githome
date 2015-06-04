c48b7e2e1be394d32d15396fe41e30517733958d
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-04 18:00:49 +0200

    Leverage clicks testing facilities in tests.

--- tests/test_githome.py
+++ tests/test_githome.py
@@ -1,3 +1,14 @@
+
+from click.testing import CliRunner
+from githome.cmd import cli
+import pytest
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
 # the most basic tests tells us whether or not we messed up any dependencies
-def test_cli_basic():
-    from githome.cmd import cli
+def test_cli_basic(runner):
+    runner.invoke(cli, ['--help'])
